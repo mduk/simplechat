8ce41f1516266f398da7be341a8103e1c09c2ebb
Daniel Kendell <daniel_kendell@macdan.net>
2012-11-19 22:51:28 +0000

    Removed unused function. Added comment dividers

--- src/simplechat_client.erl
+++ src/simplechat_client.erl
@@ -80,14 +80,13 @@ init( _ ) ->
 %===============================================================================
 % handle_cast/2
 %===============================================================================
-
 handle_cast( _, State ) -> { noreply, State }.
 
 %===============================================================================
 % handle_call/3
 %===============================================================================
-
 % Add Handler
+%-------------------------------------------------------------------------------
 handle_call( { add_handler, Module, Args }, _From, State ) ->
 	Result = gen_event:add_handler( State#state.event, Module, Args ),
 	{ reply, Result, State };
@@ -112,10 +111,14 @@ handle_call( room_list, _From, State ) ->
 		simplechat_room:info( Room )
 	end, simplechat_room_sup:rooms() ),
 	{ reply, pending, State };
+%-------------------------------------------------------------------------------
 % List of rooms the client has joined
+%-------------------------------------------------------------------------------
 handle_call( joined_rooms, _From, State = #state{ rooms = Rooms } ) ->
 	{ reply, { ok, { joined_rooms, Rooms } }, State };
+%-------------------------------------------------------------------------------
 % Join Room
+%-------------------------------------------------------------------------------
 handle_call( { join, Room }, _From, State ) ->
 	case proplists:lookup( Room, State#state.rooms ) of
 		
@@ -134,7 +137,9 @@ handle_call( { join, Room }, _From, State ) ->
 			% Return join result and update state
 			{ reply, pending, State }
 	end;
+%-------------------------------------------------------------------------------
 % Part Room
+%-------------------------------------------------------------------------------
 handle_call( { part, Room }, _From, State ) ->
 	case proplists:lookup( Room, State#state.rooms ) of
 	
@@ -150,7 +155,9 @@ handle_call( { part, Room }, _From, State ) ->
 		none ->
 			{ reply, ok, State }
 	end;
+%-------------------------------------------------------------------------------
 % Set a room topic
+%-------------------------------------------------------------------------------
 handle_call( { set_topic, Room, Topic }, _From, State ) ->
 	case proplists:lookup( Room, State#state.rooms ) of
 		
@@ -162,7 +169,9 @@ handle_call( { set_topic, Room, Topic }, _From, State ) ->
 			{ reply, { error, not_present_in_room }, State }
 			
 	end;
+%-------------------------------------------------------------------------------
 % Lock room topic
+%-------------------------------------------------------------------------------
 handle_call( { Lock, Room }, _From, State ) when Lock =:= lock_topic; Lock =:= unlock_topic ->
 	case proplists:lookup( Room, State#state.rooms ) of
 		
@@ -178,7 +187,9 @@ handle_call( { Lock, Room }, _From, State ) when Lock =:= lock_topic; Lock =:= u
 			{ reply, { error, not_present_in_room }, State }
 			
 	end;
+%-------------------------------------------------------------------------------
 % Say something in a room
+%-------------------------------------------------------------------------------
 handle_call( { say, Room, Message }, _From, State ) ->
 	case proplists:lookup( Room, State#state.rooms ) of
 		
@@ -190,62 +201,76 @@ handle_call( { say, Room, Message }, _From, State ) ->
 			{ reply, { error, not_present_in_room }, State }
 			
 	end;
+%-------------------------------------------------------------------------------
 % Shout to all rooms
+%-------------------------------------------------------------------------------
 handle_call( { shout, Message }, _From, State ) ->
 	#state{ rooms = Rooms } = State,
 	lists:foreach( fun( { _, Room } ) ->
 		simplechat_room:say( Room, Message )
 	end, Rooms ),
 	{ reply, pending, State };
+%-------------------------------------------------------------------------------
 % Quit command
+%-------------------------------------------------------------------------------
 handle_call( quit, _From, State = #state{ rooms = Rooms } ) ->
 	part_all( Rooms ),
 	gen_event:sync_notify( State#state.event, quit ),
 	{ stop, shutdown, ok, State#state{ rooms = [] } };
+%-------------------------------------------------------------------------------
 % Catch-all
+%-------------------------------------------------------------------------------
 handle_call( _Msg, _From, State ) ->
 	{ reply, { error, unknown_call }, State }.
 
 %===============================================================================
 % handle_info/2
 %===============================================================================
-
 % A room has been created
+%-------------------------------------------------------------------------------
 handle_info( E = { server_event, _ }, State ) ->
 	fire( State, E ),
 	{ noreply, State };
-
+%-------------------------------------------------------------------------------
 % A room has accepted the client's join request
+%-------------------------------------------------------------------------------
 handle_info( { room, { RoomName, RoomPid }, joined, RoomInfo }, State ) ->
 	fire( State, { joined, RoomInfo } ),
 	{ noreply, State#state{ rooms = [ { RoomName, RoomPid } | State#state.rooms ] } };
 
+%-------------------------------------------------------------------------------
 % A room has denied the client's join request
+%-------------------------------------------------------------------------------
 handle_info( { room, { RoomName, RoomPid }, denied }, State ) ->
 	fire( State, { denied, { RoomName, RoomPid } } ),
 	{ noreply, State };
-
+%-------------------------------------------------------------------------------
 % A room has been parted
+%-------------------------------------------------------------------------------
 handle_info( { room, { RoomName, RoomPid }, parted }, State ) ->
 	fire( State, { parted, { RoomName, RoomPid } } ),
 	{ noreply, State#state{ rooms = lists:delete( { RoomName, RoomPid }, State#state.rooms ) } };
-
+%-------------------------------------------------------------------------------
 % Room info
+%-------------------------------------------------------------------------------
 handle_info( { room, _, info, RoomInfo }, State ) ->
 	fire( State, { room_info, RoomInfo } ),
 	{ noreply, State };
-
+%-------------------------------------------------------------------------------
 % A room error
+%-------------------------------------------------------------------------------
 handle_info( Error = { room, _, { error, _ } }, State ) ->
 	fire( State, Error ),
 	{ noreply, State };
-
+%-------------------------------------------------------------------------------
 % All Room Events
+%-------------------------------------------------------------------------------
 handle_info( Event = { room_event, _, _ }, State ) ->
 	fire( State, Event ),
 	{ noreply, State };
-
+%-------------------------------------------------------------------------------
 % Catch All
+%-------------------------------------------------------------------------------
 handle_info( _Msg, State ) ->
 	{ noreply, State }.
 
@@ -255,14 +280,32 @@ terminate( _Reason, _State ) ->
 code_change( _Vsn, State, _Opts ) ->
 	{ ok, State }.
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Private functions
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%===============================================================================
+% Fire event to room event manager
+%===============================================================================
 fire( S, E ) ->
 	gen_event:notify( S#state.event, E ).
 
+%===============================================================================
+% Validate a nickname
+%===============================================================================
+% Nick is blank (empty binary)
+%-------------------------------------------------------------------------------
 valid_nick( <<>> ) -> false;
+%-------------------------------------------------------------------------------
+% Everything else is fine
+%-------------------------------------------------------------------------------
 valid_nick( _ ) -> true.
 	
-part_all( [] ) -> ok;
-part_all( [ { Name, Pid } | T ] ) ->
-	io:format( "Parting ~s ~p~n", [ Name, Pid ] ),
+%===============================================================================
+% Part all joined rooms
+%===============================================================================
+part_all( [ { _, Pid } | T ] ) ->
 	simplechat_room:part( Pid ),
-	part_all( T ).
+	part_all( T );
+part_all( [] ) ->
+	ok.
