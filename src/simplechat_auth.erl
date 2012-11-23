958898756b8847ba2d1708b8fd679ef1da3e7f80
Daniel Kendell <daniel_kendell@macdan.net>
2012-11-23 22:29:18 +0000

    Look for client pids in ets table, check they're still alive and if not, start a new client.

--- src/simplechat_auth.erl
+++ src/simplechat_auth.erl
@@ -14,10 +14,23 @@ ident( Nick, Password ) ->
 	Id = { Nick, Password },
 	case ets:lookup( ?TABLE, Id ) of
 		[] ->
-			{ ok, Pid } = simplechat_client_sup:start_client( Id ),
-			simplechat_client:nick( Pid, Nick ),
-			ets:insert( ?TABLE, { Id, Pid } ),
-			{ ok, Pid };
+			start_client( Id, Nick );
 		[ { Id, Pid } ] ->
-			{ ok, Pid }
+			case is_alive( Pid ) of
+				false ->
+					ets:delete( ?TABLE, Id ),
+					start_client( Id, Nick );
+				true -> 
+					{ ok, Pid }
+			end
 	end.
+
+start_client( Id, Nick ) ->
+	{ ok, Pid } = simplechat_client_sup:start_client( Id ),
+	simplechat_client:nick( Pid, Nick ),
+	ets:insert( ?TABLE, { Id, Pid } ),
+	{ ok, Pid }.
+	
+
+is_alive( Pid ) when node( Pid ) =:= node() ->
+	is_process_alive( Pid ).
